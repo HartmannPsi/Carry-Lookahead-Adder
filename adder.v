185e42b6e2480419807165d9a664a9a239d95a6e
Hartmann <hartmann_psi@sjtu.edu.cn>
2024-10-02 11:33:11 +0800

    code:完成了32位cla

--- adder.v
+++ adder.v
@@ -6,9 +6,9 @@ module Adder_4bit(
     output c_out
 );
 
-wire [3:0] g;
-wire [3:0] p;
-wire [3:0] c;
+wire[3:0] g;
+wire[3:0] p;
+wire[3:0] c;
 
 assign g = a & b;
 assign p = a ^ b;
@@ -20,5 +20,98 @@ assign c[3] = g[2] | (p[2] & (g[1] | (p[1] & (g[0] | (p[0] & c[0])))));
 
 assign sum = p ^ c;
 assign c_out = g[3] | (p[3] & (g[2] | (p[2] & (g[1] | (p[1] & (g[0] | (p[0] & c[0])))))));
+
+endmodule
+
+
+module Adder_16bit(
+    input[15:0] a,
+    input[15:0] b,
+    input c_in,
+    output[15:0] sum,
+    output c_out
+);
+
+wire[3:0] carry;
+assign carry[0] = c_in;
+
+Adder_4bit adder0(
+    .a(a[3:0]),
+    .b(b[3:0]),
+    .c_in(carry[0]),
+    .sum(sum[3:0]),
+    .c_out(carry[1])
+);
+
+Adder_4bit adder1(
+    .a(a[7:4]),
+    .b(b[7:4]),
+    .c_in(carry[1]),
+    .sum(sum[7:4]),
+    .c_out(carry[2])
+);
+
+Adder_4bit adder2(
+    .a(a[11:8]),
+    .b(b[11:8]),
+    .c_in(carry[2]),
+    .sum(sum[11:8]),
+    .c_out(carry[3])
+);
+
+Adder_4bit adder3(
+    .a(a[15:12]),
+    .b(b[15:12]),
+    .c_in(carry[3]),
+    .sum(sum[15:12]),
+    .c_out(c_out)
+);
+
+
+endmodule
+
+module Adder_32bit(
+    input[31:0] a,
+    input[31:0] b,
+    input c_in,
+    output[31:0] sum,
+    output c_out
+);
+
+wire carry;
+
+Adder_16bit adder0(
+    .a(a[15:0]),
+    .b(b[15:0]),
+    .c_in(c_in),
+    .sum(sum[15:0]),
+    .c_out(carry)
+);
+
+Adder_16bit adder1(
+    .a(a[31:16]),
+    .b(b[31:16]),
+    .c_in(carry),
+    .sum(sum[31:16]),
+    .c_out(c_out)
+);
+
+endmodule
+
+module Add(
+    input[31:0] a,
+    input[31:0] b,
+    output[31:0] sum,
+    output carry
+);
+
+Adder_32bit adder(
+    .a(a),
+    .b(b),
+    .c_in(1'b0),
+    .sum(sum),
+    .c_out(carry)
+);
+
 
 endmodule
--- testbench.v
+++ testbench.v
@@ -1,34 +1,31 @@
 `timescale 1ns/1ps
 
 module testbench;
-    reg [3:0] a;
-    reg [3:0] b;
-    reg c_in;
-    wire [3:0] sum;
-    wire c_out;
+    reg [31:0] a;
+    reg [31:0] b;
+    wire [31:0] sum;
+    wire carry;
 
     // 实例化被测试的模块
-    Adder_4bit uut (
+    Add uut (
         .a(a),
         .b(b),
-        .c_in(c_in),
         .sum(sum),
-        .c_out(c_out)
+        .carry(carry)
     );
 
     initial begin
         // 初始化输入
-        a = 4'b0000;
-        b = 4'b0000;
-        c_in = 1'b0;
+        a = 32'd0;
+        b = 32'd0;
 
         // 监视信号变化
-        $monitor("a = %b, b = %b, c_in = %b, sum = %b, c_out = %b", a, b, c_in, sum, c_out);
+        $monitor("a = %d, b = %d, sum = %d, carry = %b", a, b, sum, carry);
 
         // 应用测试向量
-        #10 a = 4'b0101; b = 4'b0011; c_in = 1'b0;
-        #10 a = 4'b1100; b = 4'b1010; c_in = 1'b1;
-        #10 a = 4'b1111; b = 4'b1111; c_in = 1'b1;
+        #10 a = 32'd15; b = 32'd10;
+        #10 a = 32'd123456789; b = 32'd987654321;
+        #10 a = 32'd4294967295; b = 32'd1; // 最大32位无符号整数加1
         #10 $finish;
     end
 endmodule
