62a78c05833da8f6a93096a6dc4d043d5de7a5e1
Hartmann <hartmann_psi@sjtu.edu.cn>
2024-10-17 18:36:51 +0800

    bugfix:修复了时延过高的bug

--- adder.v
+++ adder.v
@@ -13,13 +13,16 @@ wire[3:0] c;
 assign g = a & b;
 assign p = a ^ b;
 assign c[0] = c_in;
+
 assign c[1] = g[0] | (p[0] & c[0]);
-assign c[2] = g[1] | (p[1] & (g[0] | (p[0] & c[0])));
-assign c[3] = g[2] | (p[2] & (g[1] | (p[1] & (g[0] | (p[0] & c[0])))));
+
+assign c[2] = g[1] | (p[1] & g[0]) | (p[1] & p[0] & c[0]);
+
+assign c[3] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & c[0]);
 
 
 assign sum = p ^ c;
-assign c_out = g[3] | (p[3] & (g[2] | (p[2] & (g[1] | (p[1] & (g[0] | (p[0] & c[0])))))));
+assign c_out = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & c[0]);
 
 endmodule
 
